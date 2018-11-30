f3eb35371b56f168cf7b826d3ea9175d75ae96dd
vajda <vajdaandris56@yahoo.com>
2018-11-30 19:38:19 +0200

    Splash screen added

--- app/src/main/java/com/example/andras/myapplication/MainActivity.java
+++ app/src/main/java/com/example/andras/myapplication/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.andras.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Duration of wait
+     **/
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* New Handler to start the LoginActivity-Activity
+        * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run () {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        },SPLASH_DISPLAY_LENGTH);
     }
 }
